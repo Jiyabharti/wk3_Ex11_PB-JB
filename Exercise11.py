6e8a851fc3862d2889990f324b0512b36d4cee3c
Jiya Bharti <Jiyabharti@oulook.com>
2024-02-03 21:09:44 +0000

    checking Jiya's side

--- Exercise11.py
+++ Exercise11.py
@@ -52,3 +52,6 @@ print(BelgiumSplit)
 # if we had larger data, how could we convert the data to a collection quickly and identify the items to add?
 
 # Alternative: slice the data? separate the collection into two.
+# checkjiya
+
+
